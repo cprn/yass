f2e2255f7131036566889f146bcc64165d3568e5
Cyprian Guerra <cyprian.guerra@gmail.com>
2015-02-08 05:38:41 +0100

    screen is drawn from top to bottom so rotated ship
    - FIX: ship rotation
    - abstracted Width and Height to W & H globals
    - event -> e
    - quit on escape
    - input grab
    - rotated ship

--- rotate.py
+++ rotate.py
@@ -1,34 +1,39 @@
 import sys
 import pygame
 import math
-from pygame.locals import QUIT, MOUSEBUTTONDOWN
+from pygame.locals import QUIT, MOUSEBUTTONDOWN, KEYDOWN, K_ESCAPE
+
+W = 800
+H = 600
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((W, H))
+
 stars = pygame.image.load('background.jpg').convert_alpha()
 cross = pygame.image.load('crosshair.png').convert_alpha()
 ship = pygame.image.load('spaceship.png').convert_alpha()
 clock = pygame.time.Clock()
+
 pygame.mouse.set_visible(False)
+pygame.event.set_grab(True)
 
 while True:
-    clock.tick(10)
-    for event in pygame.event.get():
-        if event.type == QUIT:
+    clock.tick(60)
+    for e in pygame.event.get():
+        if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
             pygame.quit()
             sys.exit()
-        elif event.type == MOUSEBUTTONDOWN and event.button == 1:
+        elif e.type == MOUSEBUTTONDOWN and e.button == 1:
             print('*pew pew!*')
-        elif event.type == MOUSEBUTTONDOWN and event.button == 3:
+        elif e.type == MOUSEBUTTONDOWN and e.button == 3:
             print('*swoosh!*')
 
     screen.blit(stars, (0, 0))
     pos = pygame.mouse.get_pos()
     screen.blit(cross, (pos))
-    angle = 180+math.atan2(pos[0]-400, pos[1]-300)*180/math.pi
-    print(round(angle))
-    rotimage = pygame.transform.rotate(ship, angle)
-    rect = rotimage.get_rect(center=(400, 300))
-    screen.blit(rotimage, rect)
+    angle = math.atan2(pos[0]-W/2, pos[1]-H/2)*180/math.pi
+    player = pygame.transform.rotate(ship, angle)
+    rect = player.get_rect(center=(W/2, H/2))
+    screen.blit(player, rect)
     pygame.display.update()
